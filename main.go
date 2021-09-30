2f6043c4a773fc6cdbc9202c359744bb3e1dcf5f
alireza naserpoor <anaserpoor@yahoo.com>
2021-09-30 15:12:01 +0330

    add loggings

--- main.go
+++ main.go
@@ -4,24 +4,32 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
-	input := [2]byte{}
 	server,_ := net.Listen("tcp", fmt.Sprintf("%v",os.Args[1]))
 	fmt.Println("Server Started")
 
 	for {
 		conn,_ := server.Accept()
-		fmt.Println("Connection Started")
-		conn.Read(input[:])
-		fmt.Println(input)
-		if input[0] == 4 && input[1] == 1 {
-			go socks4(conn)
-		} else if input[0] == 5 && input[1] > 0 {
-			go socks5(conn, input[1])
-		} else {
-			conn.Close()
-		}
+		go func() {
+			input := make([]byte,2)
+			fmt.Println("Connection Started")
+			conn.SetReadDeadline(time.Now().Add(time.Second*2))
+			_,err := conn.Read(input[:])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(input)
+			if input[0] == 4 && input[1] == 1 {
+				go socks4(conn)
+			} else if input[0] == 5 && input[1] > 0 {
+				go socks5(conn, input[1])
+			} else {
+				conn.Close()
+			}
+		}()
 	}
 }
