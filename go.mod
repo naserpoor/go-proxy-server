4c8687974e9780e0e9e6d6ae83a3fc5e3bd97a20
alireza naserpoor <anaserpoor@yahoo.com>
2021-09-30 20:26:47 +0330

    check socks connection

--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module go-proxy-server
 go 1.17
 
 require (
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5 // indirect
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
 	gopkg.in/elazarl/goproxy.v1 v1.0.0-20180725130230-947c36da3153 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"fmt"
+	goSocks5 "github.com/armon/go-socks5"
 	"gopkg.in/elazarl/goproxy.v1"
 	"log"
-	"net"
 	"net/http"
-	"os"
 )
 
 func main() {
-	server,_ := net.Listen("tcp", fmt.Sprintf("%v",os.Args[1]))
-	fmt.Println("Server Started")
+	//server,_ := net.Listen("tcp", fmt.Sprintf("%v",os.Args[1]))
+	//fmt.Println("Server Started")
 
 	//http.HandleFunc("/",http_proxy)
 	//http.ListenAndServe(":12012",nil)
@@ -20,24 +18,37 @@ func main() {
 		log.Fatal(http.ListenAndServe(":12012", proxy))
 	}()
 
-	for {
-		conn,_ := server.Accept()
-		go func() {
-			input := make([]byte,2)
-			fmt.Println("Connection Started")
-			_,err := conn.Read(input[:])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(input)
-			if input[0] == 4 && input[1] == 1 {
-				go socks4(conn)
-			} else if input[0] == 5 && input[1] > 0 {
-				go socks5(conn, input[1])
-			} else {
-				conn.Close()
-			}
-		}()
-	}
+	go func() {
+		conf := &goSocks5.Config{}
+		server, err := goSocks5.New(conf)
+		if err != nil {
+			panic(err)
+		}
+
+		// Create SOCKS5 proxy on localhost port 8000
+		if err := server.ListenAndServe("tcp", ":12013"); err != nil {
+			panic(err)
+		}
+	}()
+
+	//for {
+	//	conn,_ := server.Accept()
+	//	go func() {
+	//		input := make([]byte,2)
+	//		fmt.Println("Connection Started")
+	//		_,err := conn.Read(input[:])
+	//		if err != nil {
+	//			fmt.Println(err)
+	//			return
+	//		}
+	//		fmt.Println(input)
+	//		if input[0] == 4 && input[1] == 1 {
+	//			go socks4(conn)
+	//		} else if input[0] == 5 && input[1] > 0 {
+	//			go socks5(conn, input[1])
+	//		} else {
+	//			conn.Close()
+	//		}
+	//	}()
+	//}
 }
