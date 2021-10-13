921c399eb1e21a5caff08528636375cf1454c045
alireza naserpoor <anaserpoor@yahoo.com>
2021-10-13 13:05:18 +0330

    code refactor

--- socks_server.go
+++ socks_server.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"github.com/google/logger"
-	"math"
 	"net"
-	"strconv"
-	"strings"
 )
 
 type SocksServer struct {
@@ -53,53 +49,4 @@ func (server *SocksServer) Start() {
 		connection := NewConnection(conn)
 		go connection.StartConnectionProcess()
 	}
-}
-
-func ParseIpStr(ipStr string) (uint32, error) {
-	const errorWrongFormat = "Ip wrong format"
-	if strings.ToLower(ipStr) == "localhost" {
-		ipStr = "127.0.0.1"
-	}
-	ipParts := strings.Split(ipStr, ".")
-	if len(ipParts) != 4 {
-		return 0, errorT{error: errorWrongFormat}
-	}
-	ipBytes := make([]byte, 4)
-	for idx, part := range ipParts {
-		res, er := strconv.Atoi(part)
-		if er != nil || res > 255 || res < 0 {
-			return 0, errorT{error: errorWrongFormat}
-		}
-		ipBytes[idx] = byte(res)
-	}
-	return binary.LittleEndian.Uint32(ipBytes), nil
-}
-
-func ParsePortStr(portStr string) (uint16, error) {
-	port, er := strconv.Atoi(portStr)
-	if er != nil {
-		return 0, er
-	}
-	if port > math.MaxUint16 || port < 0 {
-		return 0, errorT{"Wrong Port Number:" + portStr}
-	}
-	return uint16(port), nil
-}
-
-func ParseIpUint32(ipUint uint32) string {
-	ipBytes := make([]byte, 4)
-	binary.LittleEndian.PutUint32(ipBytes, ipUint)
-	return fmt.Sprintf("%v.%v.%v.%v", ipBytes[0], ipBytes[1], ipBytes[2], ipBytes[3])
-}
-
-func FormatIpAndPort(ip uint32, port uint16) string {
-	return fmt.Sprintf("%v:%v", ParseIpUint32(ip), port)
-}
-
-type errorT struct {
-	error string
-}
-
-func (a errorT) Error() string {
-	return a.error
 }
--- utils.go
+++ utils.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/binary"
+	"fmt"
 	"github.com/google/logger"
+	"math"
 	"net"
 	"strconv"
+	"strings"
 )
 
 func makeInput(size int) []byte {
@@ -136,4 +140,53 @@ func extractSizePrefixedString(conn net.Conn) ([]byte,error) {
 		result = append(result, input...)
 	}
 	return result, nil
+}
+
+func ParseIpStr(ipStr string) (uint32, error) {
+	const errorWrongFormat = "Ip wrong format"
+	if strings.ToLower(ipStr) == "localhost" {
+		ipStr = "127.0.0.1"
+	}
+	ipParts := strings.Split(ipStr, ".")
+	if len(ipParts) != 4 {
+		return 0, errorT{error: errorWrongFormat}
+	}
+	ipBytes := make([]byte, 4)
+	for idx, part := range ipParts {
+		res, er := strconv.Atoi(part)
+		if er != nil || res > 255 || res < 0 {
+			return 0, errorT{error: errorWrongFormat}
+		}
+		ipBytes[idx] = byte(res)
+	}
+	return binary.LittleEndian.Uint32(ipBytes), nil
+}
+
+func ParsePortStr(portStr string) (uint16, error) {
+	port, er := strconv.Atoi(portStr)
+	if er != nil {
+		return 0, er
+	}
+	if port > math.MaxUint16 || port < 0 {
+		return 0, errorT{"Wrong Port Number:" + portStr}
+	}
+	return uint16(port), nil
+}
+
+func ParseIpUint32(ipUint uint32) string {
+	ipBytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(ipBytes, ipUint)
+	return fmt.Sprintf("%v.%v.%v.%v", ipBytes[0], ipBytes[1], ipBytes[2], ipBytes[3])
+}
+
+func FormatIpAndPort(ip uint32, port uint16) string {
+	return fmt.Sprintf("%v:%v", ParseIpUint32(ip), port)
+}
+
+type errorT struct {
+	error string
+}
+
+func (a errorT) Error() string {
+	return a.error
 }
